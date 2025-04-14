db487aa434f7836b365c56d059d95c9524d1b656
lpdaquila <lpd.lucas@outlook.com>
2025-04-14 18:54:07 -0300

    Changes to be committed: 	modified:   src/app/(project)/dashboard/page.tsx 	modified:   src/app/(project)/login/page.tsx 	new file:   src/app/actions/handle-auth.ts 	new file:   src/app/api/auth/[...nextauth]/route.ts 	new file:   src/app/lib/auth.ts

--- src/app/(project)/dashboard/page.tsx
+++ src/app/(project)/dashboard/page.tsx
@@ -1,5 +1,28 @@
-export default function Dashboard() {
+import { handleAuth } from "@/app/actions/handle-auth";
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+    const session = await auth();
+
+    if (!session) {
+        redirect("/login");
+    }
+
     return <div className="flex h-screen items-center justify-center bg-gray-200">
         <h1 className="text-4xl font-bold text-gray-800">Protected Dashboard</h1>
+        <p>
+            {session.user?.name ? `Hello ${session.user.name}` : "Hello User"}
+        </p>
+        {
+            session?.user?.email && (
+                <form action={handleAuth}>
+                    <button 
+                    type="submit"
+                    className="border rounded-md"
+                    >Logout</button>
+                </form>
+            )
+        }
     </div>;
 }
--- src/app/(project)/login/page.tsx
+++ src/app/(project)/login/page.tsx
@@ -1,5 +1,12 @@
+import { handleAuth } from "@/app/actions/handle-auth";
+
 export default function Home() {
     return <div className="flex h-screen items-center justify-center bg-gray-200">
-      <h1 className="text-4xl font-bold text-gray-800">Login</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-10">Login</h1>
+      <form
+      action={handleAuth}
+    >
+      <button type="submit" className="border rounded-md">Signin with Google</button>
+    </form>
     </div>;
   }
